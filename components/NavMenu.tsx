6509117afe271a0d4472683191eee22fbd9458cf
Nithish Gowda <nithishgowda0608@gmail.com>
2025-11-09 09:13:15 +0530

    fix: import React in multiple components and clean up unused imports

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,14 +1,11 @@
 import Link from "next/link";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { cn } from "@/lib/utils";
 
 interface NavMenuProps {
   className?: string;
