f83160a245199ad6f0a3b6d6cff99891a77c40fa
Nithish Gowda <nithishgowda0608@gmail.com>
2025-11-10 12:10:10 +0530

    feat: add NextTopLoader for improved page loading experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import { ThemeScript } from "@/components/theme/ThemeScript";
+import NextTopLoader from "nextjs-toploader";
 import React from "react";
 
 const geistSans = Geist({
@@ -35,6 +36,17 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <NextTopLoader
+            color="#2299dd"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #2299dd,0 0 5px #2299dd"
+          />
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
